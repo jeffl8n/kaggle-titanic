58439c14ce65b5ab261229abb1e8c6358b85b58f
Jeff Layton <jeffl8n@gmail.com>
2015-07-31 12:33:13 -0400

    Implementing a random forest

---
+++ dataquest_mission75.py
@@ -0,0 +1,37 @@
+import pandas
+
+# We can use the pandas library in python to read in the csv file.
+# This creates a pandas dataframe and assigns it to the titanic variable.
+titanic = pandas.read_csv("titanic_train.csv")
+
+# The titanic variable is available here.
+titanic["Age"] = titanic["Age"].fillna(titanic["Age"].median())
+
+# Replace all the occurences of male with the number 0.
+titanic.loc[titanic["Sex"] == "male", "Sex"] = 0
+
+# Replace all the occurences of female with the number 1.
+titanic.loc[titanic["Sex"] == "female", "Sex"] = 1
+
+titanic["Embarked"] = titanic["Embarked"].fillna("S")
+
+titanic.loc[titanic["Embarked"] == "S", "Embarked"] = 0
+titanic.loc[titanic["Embarked"] == "C", "Embarked"] = 1
+titanic.loc[titanic["Embarked"] == "Q", "Embarked"] = 2
+
+
+from sklearn import cross_validation
+from sklearn.ensemble import RandomForestClassifier
+
+predictors = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
+
+# Initialize our algorithm with the default paramters
+# n_estimators is the number of trees we want to make
+# min_samples_split is the minimum number of rows we need to make a split
+# min_samples_leaf is the minimum number of samples we can have at the place where a tree branch ends (the bottom points of the tree)
+alg = RandomForestClassifier(random_state=1, n_estimators=10, min_samples_split=2, min_samples_leaf=1)
+
+# Compute the accuracy score for all the cross validation folds.  (much simpler than what we did before!)
+scores = cross_validation.cross_val_score(alg, titanic[predictors], titanic["Survived"], cv=3)
+# Take the mean of the scores (because we have one for each fold)
+print(scores.mean())
