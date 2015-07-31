bff1eb3ac7447eabb56aecc18fd4e7de52d7ed24
Jeff Layton <jeffl8n@gmail.com>
2015-07-31 12:48:02 -0400

    Finding the best features

--- dataquest_mission75.py
+++ dataquest_mission75.py
@@ -106,3 +106,31 @@ family_ids[titanic["FamilySize"] < 3] = -1
 print(pandas.value_counts(family_ids))
 
 titanic["FamilyId"] = family_ids
+
+import numpy as np
+from sklearn.feature_selection import SelectKBest, f_classif
+import matplotlib.pyplot as plt
+
+predictors = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked", "FamilySize", "Title", "FamilyId"]
+
+# Perform feature selection
+selector = SelectKBest(f_classif, k=5)
+selector.fit(titanic[predictors], titanic["Survived"])
+
+# Get the raw p-values for each feature, and transform from p-values into scores
+scores = -np.log10(selector.pvalues_)
+
+# Plot the scores.  See how "Pclass", "Sex", "Title", and "Fare" are the best?
+plt.bar(range(len(predictors)), scores)
+plt.xticks(range(len(predictors)), predictors, rotation='vertical')
+plt.show()
+
+# Pick only the four best features.
+predictors = ["Pclass", "Sex", "Fare", "Title"]
+
+alg = RandomForestClassifier(random_state=1, n_estimators=150, min_samples_split=8, min_samples_leaf=4)
+
+# Compute the accuracy score for all the cross validation folds.  (much simpler than what we did before!)
+scores = cross_validation.cross_val_score(alg, titanic[predictors], titanic["Survived"], cv=3)
+# Take the mean of the scores (because we have one for each fold)
+print(scores.mean())
