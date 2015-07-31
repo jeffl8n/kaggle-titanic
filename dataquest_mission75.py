a8addf709ef7f859b7e0f5dd6d9b74892fe6168a
Jeff Layton <jeffl8n@gmail.com>
2015-07-31 12:52:03 -0400

    Ensembling

--- dataquest_mission75.py
+++ dataquest_mission75.py
@@ -123,7 +123,7 @@ scores = -np.log10(selector.pvalues_)
 # Plot the scores.  See how "Pclass", "Sex", "Title", and "Fare" are the best?
 plt.bar(range(len(predictors)), scores)
 plt.xticks(range(len(predictors)), predictors, rotation='vertical')
-plt.show()
+#plt.show()
 
 # Pick only the four best features.
 predictors = ["Pclass", "Sex", "Fare", "Title"]
@@ -134,3 +134,45 @@ alg = RandomForestClassifier(random_state=1, n_estimators=150, min_samples_split
 scores = cross_validation.cross_val_score(alg, titanic[predictors], titanic["Survived"], cv=3)
 # Take the mean of the scores (because we have one for each fold)
 print(scores.mean())
+
+from sklearn.linear_model import LogisticRegression
+# Sklearn also has a helper that makes it easy to do cross validation
+from sklearn.cross_validation import KFold
+from sklearn.ensemble import GradientBoostingClassifier
+import numpy as np
+
+# The algorithms we want to ensemble.
+# We're using the more linear predictors for the logistic regression, and everything with the gradient boosting classifier.
+algorithms = [
+    [GradientBoostingClassifier(random_state=1, n_estimators=25, max_depth=3), ["Pclass", "Sex", "Age", "Fare", "Embarked", "FamilySize", "Title", "FamilyId"]],
+    [LogisticRegression(random_state=1), ["Pclass", "Sex", "Fare", "FamilySize", "Title", "Age", "Embarked"]]
+]
+
+# Initialize the cross validation folds
+kf = KFold(titanic.shape[0], n_folds=3, random_state=1)
+
+predictions = []
+for train, test in kf:
+    train_target = titanic["Survived"].iloc[train]
+    full_test_predictions = []
+    # Make predictions for each algorithm on each fold
+    for alg, predictors in algorithms:
+        # Fit the algorithm on the training data.
+        alg.fit(titanic[predictors].iloc[train,:], train_target)
+        # Select and predict on the test fold.  
+        # The .astype(float) is necessary to convert the dataframe to all floats and avoid an sklearn error.
+        test_predictions = alg.predict_proba(titanic[predictors].iloc[test,:].astype(float))[:,1]
+        full_test_predictions.append(test_predictions)
+    # Use a simple ensembling scheme -- just average the predictions to get the final classification.
+    test_predictions = (full_test_predictions[0] + full_test_predictions[1]) / 2
+    # Any value over .5 is assumed to be a 1 prediction, and below .5 is a 0 prediction.
+    test_predictions[test_predictions <= .5] = 0
+    test_predictions[test_predictions > .5] = 1
+    predictions.append(test_predictions)
+
+# Put all the predictions together into one array.
+predictions = np.concatenate(predictions, axis=0)
+
+# Compute accuracy by comparing to the training data.
+accuracy = sum(predictions[predictions == titanic["Survived"]]) / len(predictions)
+print(accuracy)
