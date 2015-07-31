d61069fa6eeecf7d9f9211438a4aa1f56218d7e2
Jeff Layton <jeffl8n@gmail.com>
2015-07-31 12:25:01 -0400

    Step 5

--- dataquest_mission74.py
+++ dataquest_mission74.py
@@ -30,3 +30,29 @@ titanic["Embarked"] = titanic["Embarked"].fillna("S")
 titanic.loc[titanic["Embarked"] == "S", "Embarked"] = 0
 titanic.loc[titanic["Embarked"] == "C", "Embarked"] = 1
 titanic.loc[titanic["Embarked"] == "Q", "Embarked"] = 2
+
+# Import the linear regression class
+from sklearn.linear_model import LinearRegression
+# Sklearn also has a helper that makes it easy to do cross validation
+from sklearn.cross_validation import KFold
+
+# The columns we'll use to predict the target
+predictors = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
+
+# Initialize our algorithm class
+alg = LinearRegression()
+# Generate cross validation folds for the titanic dataset.  It return the row indices corresponding to train and test.
+# We set random_state to ensure we get the same splits every time we run this.
+kf = KFold(titanic.shape[0], n_folds=3, random_state=1)
+
+predictions = []
+for train, test in kf:
+    # The predictors we're using the train the algorithm.  Note how we only take the rows in the train folds.
+    train_predictors = (titanic[predictors].iloc[train,:])
+    # The target we're using to train the algorithm.
+    train_target = titanic["Survived"].iloc[train]
+    # Training the algorithm using the predictors and target.
+    alg.fit(train_predictors, train_target)
+    # We can now make predictions on the test fold
+    test_predictions = alg.predict(titanic[predictors].iloc[test,:])
+    predictions.append(test_predictions)
